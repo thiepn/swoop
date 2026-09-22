e7568f426facd445130a678c5f9c3b1d3ad69cda
thiepn <junsosmart@gmail.com>
2026-09-22 17:33:53 +0200

    Refresh cache for Swoop v1.3.0 UI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="swoop-v1.2.0";
+const CACHE="swoop-v1.3.0";
 const CORE=["./","./index.html","./manifest.webmanifest","./icon.svg","./icon-maskable.svg"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));
